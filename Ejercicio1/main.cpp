24457450e043a93489e0bfdf8c49ab94c0d4c983
Mario Leguizamo <mario.leguizamo@gmail.com>
2020-05-17 17:34:23 -0500

    Iniciamos repositorio

---
+++ Ejercicio1/main.cpp
@@ -0,0 +1,99 @@
+//
+//  main.cpp
+//  Ejercicio1
+//
+//  Created by Mario Leguízamo Vega on 15/05/20.
+//  Copyright © 2020 Mario Leguízamo Vega. All rights reserved.
+//
+
+#include <iostream>
+//#define n 100
+#define pi 3.1416
+
+using namespace std;
+
+float calcularPerimetroPoligono(float tamañoLado, int numeroLados){
+    float resultado;
+    
+    resultado = tamañoLado*numeroLados;
+    
+    return resultado;
+    //return tamañoLado*numeroLados;
+}
+
+void identificarPoligono(int numeroLados){
+    //comparadores == <= >= < > !=
+    switch (numeroLados) {
+        case 3:
+            cout << "Es un Triangulo\n";
+            break;
+        case 4:
+            cout << "Es un Cuadrado\n";
+            break;
+        case 5:
+            cout << "Es un Pentagono\n";
+            break;
+        case 6:
+            cout << "Es un Hexagono\n";
+            break;
+        case 7:
+            cout << "Es un Heptagono\n";
+            break;
+        case 8:
+            cout << "Es un Octagono\n";
+            break;
+            
+        default:
+            
+            break;
+    }
+    
+    /*
+    if (numeroLados == 3) cout << "Es un Triangulo\n";
+    
+    else if (numeroLados == 4)
+        cout << "Es un Cuadrado\n";
+
+    
+    else if (numeroLados == 5) {
+        cout << "Es un Pentagono\n";
+    }
+    
+    else if (numeroLados == 6) {
+        cout << "Es un Hexagono\n";
+    }
+    
+    else if (numeroLados == 7) {
+        cout << "Es un Heptagono\n";
+    }
+    
+    else if (numeroLados == 8) {
+        cout << "Es un Octagono\n";
+    }*/
+}
+
+int main() {
+    int numeroLados;
+    float tamañoLado;
+    float resultado;
+    
+    cout << "Cuantos lados tiene el poligono?\n";
+    cin >> numeroLados;
+    
+    identificarPoligono(numeroLados);
+    cout << "Cuanto mide cada lado?\n";
+    cin >> tamañoLado;
+    resultado = calcularPerimetroPoligono(tamañoLado, numeroLados);
+    cout << "El perimetro es: " << resultado << endl;
+    
+    return 0;
+}
+
+
+
+/*
+float sumarDosValores(int tamañoLado, int numeroLados){
+    float resultado;
+    
+    
+}*/
